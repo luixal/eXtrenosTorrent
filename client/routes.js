12751af63653159c52495465d4ac81cb3cdbd3e1
luixal <luixal@gmail.com>
2016-05-08 23:20:45 +0200

    Removes contact form. Unused (inherited from bootstrap project)

--- client/routes.js
+++ client/routes.js
@@ -12,20 +12,3 @@ Router.route(
     }
   }
 );
-
-Router.route(
-  '/contact',
-  {
-    // subscriptions: function() {
-    //   return Meteor.subscribe('stats');
-    // },
-
-    action: function() {
-      if (this.ready()) {
-        this.render('contactForm');
-      } else {
-        console.log('loading contact template...');
-      }
-    }
-  }
-);
